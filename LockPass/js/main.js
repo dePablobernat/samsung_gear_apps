b41707066dff0996335b686c24ec62b706e74102
Bernardo de Pablo Gari <bernardo.depablo-gari@lbigroup.es>
2016-12-01 15:40:38 +0100

    Initial commit

---
+++ LockPass/js/main.js
@@ -0,0 +1,180 @@
+window.onload = function() {
+
+    var eCW = new CustomEvent('rotarydetent', {detail: {'direction': 'CW'}});
+    var eCCW = new CustomEvent('rotarydetent', {detail: {'direction': 'CCW'}});
+    var tester = document.querySelector('.tester');
+    var tester2 = document.querySelector('.tester2');
+
+    tester.addEventListener('click', function(e){
+      e.preventDefault();
+      document.dispatchEvent(eCW);
+    });
+    tester2.addEventListener('click', function(e){
+      e.preventDefault();
+      document.dispatchEvent(eCCW);
+    });
+
+
+    var number = document.querySelector('.number'),
+        circle = document.querySelector('.circle'),
+        body = document.querySelector('body'),
+        session = localStorage.getItem('Session'),
+        storeSecret = localStorage.getItem('secreto'),
+        storedPassword = localStorage.getItem('combinationCode'),
+        counter = 0,
+        eventCount = 0,
+        NumDegrees = 0,
+        CombinationNumber = '',
+        SecretMessage = '',
+        StoredCombinationCode = '',
+        TrialCombination = '';
+
+    var ResetWheel = function(msg) {
+        number.innerHTML = msg;
+        setTimeout(function() {
+            ResetCircle();
+            Rotator(0);
+        }, 1000);
+    };
+    var ResetCircle = function() {
+        NumDegrees = 0;
+        counter = 0;
+        number.innerHTML = counter;
+    };
+    var Rotator = function(NumDegrees) {
+        circle.style.webkitTransform = 'rotate(' + NumDegrees + 'deg)';
+        circle.style.transform = 'rotate(' + NumDegrees + 'deg)';
+    };
+    var incrementNumDeg = function() {
+        NumDegrees += 15;
+        console.log('Increment: ', NumDegrees);
+        Rotator(NumDegrees);
+        counter++;
+        if (number.innerHTML === '23') {
+            counter = 0;
+        }
+    };
+    var decreaseNumDeg = function() {
+        NumDegrees -= 15;
+        console.log('Decrease: ', NumDegrees);
+        Rotator(NumDegrees);
+        counter--;
+        if (number.innerHTML === '0') {
+            counter = 23;
+        }
+    };
+    var PasswordChange = function() {
+        var changePass = confirm('¿Cambiar Pass?');
+        if (changePass === true) {
+            body.className = '';
+            StoredCombinationCode = '';
+            eventCount = 0;
+            localStorage.clear();
+        } else {
+            eventCount = 0;
+        }
+    };
+    var SecretChange = function() {
+        var changeSecret = confirm('¿Cambiar Secreto?');
+        if (changeSecret === true) {
+            body.className = '';
+            StoredCombinationCode = '';
+            eventCount = 0;
+            localStorage.clear();
+        } else {
+            eventCount = 0;
+        }
+    };
+
+    if (session) {
+        body.className = 'stored';
+    } else {
+        alert('1 \nROTA para\n seleccionar un\n número');
+        alert('2 \nPULSA para\n guardar');
+    }
+    // add eventListener for tizenhwkey
+    document.addEventListener('tizenhwkey', function(e) {
+        if (e.keyName === 'back') {
+            try {
+                tizen.application.getCurrentApplication().exit();
+            } catch (ignore) {}
+        }
+    });
+
+    document.addEventListener('rotarydetent', function(ev) {
+        /* Get the direction value from the event */
+        var direction = ev.detail.direction;
+
+        //Turn ClockWise
+        if (direction === 'CW') {
+            incrementNumDeg();
+        }
+        //Turn Counter Clock Wise
+        else if (direction === 'CCW') {
+            decreaseNumDeg();
+        }
+
+        number.innerHTML = counter;
+        CombinationNumber = '';
+        CombinationNumber += number.textContent;
+    });
+
+
+    number.addEventListener('click', function() {
+        if (CombinationNumber === '') {
+            CombinationNumber = '0';
+        }
+        eventCount++;
+        if (body.className === 'stored') {
+            if (eventCount === 1) {
+                TrialCombination = CombinationNumber;
+                ResetWheel('👌');
+            }
+            if (eventCount === 2) {
+                TrialCombination += CombinationNumber;
+                ResetWheel('👌');
+            } else if (eventCount === 3) {
+                TrialCombination += CombinationNumber;
+                if (TrialCombination === storedPassword) {
+                    //Show storeSecret Message
+                    alert('Tu Secreto: \n' + SecretMessage);
+                    //Ask if we want to change Password
+                    PasswordChange();
+                    SecretChange();
+                    ResetWheel('👌');
+                } else {
+                    alert('INCORRECTO,\n Vuelve a probar');
+                    eventCount = 0;
+                    ResetWheel('🚫');
+                }
+                console.log(TrialCombination);
+                CombinationNumber = '';
+            }
+        } else {
+            StoredCombinationCode += CombinationNumber;
+            if (eventCount === 3) {
+                alert('Tu \n código secreto:\n' + StoredCombinationCode);
+
+                body.className = 'stored';
+                eventCount = 0;
+
+                if (!storeSecret) {
+                    SecretMessage = prompt('¿Tu Secreto?: ');
+                    localStorage.setItem('secreto', SecretMessage);
+                    storeSecret = localStorage.getItem('secreto');
+                } else {
+                    SecretMessage = storeSecret;
+                }
+                localStorage.setItem('combinationCode', StoredCombinationCode);
+                localStorage.setItem('Session', true);
+
+                storedPassword = localStorage.getItem('combinationCode');
+                session = localStorage.getItem('Session');
+
+                console.log(storedPassword);
+                CombinationNumber = '';
+            }
+            ResetWheel('👌');
+        }
+    });
+};
---
+++ REAMDE.md
@@ -0,0 +1,7 @@
+# samsung-gear-apps
+Apps targeting Gear S2 smartwatch I submitted as part of the "Devices per apps" (Dispositivos por Apps 2016) Samsung contest.
+
+* LockPass: Keep your secrets safe from anyone in your samsung gear with this combination lock. Just save a 3 number combination and keep your secret safe from anyone.
+
+![alt esnake_thumb](https://github.com/imartinez/samsung-gear-apps/blob/master/image.png?raw=true)
+
